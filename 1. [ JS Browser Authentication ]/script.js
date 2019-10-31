d276420b568601654ae2ac116dc1f395d284f14b
Mike2142 <49782969+Mike2142@users.noreply.github.com>
2019-10-31 17:21:04 +0300

    Update script.js

--- 1. [ JS Browser Authentication ]/script.js
+++ 1. [ JS Browser Authentication ]/script.js
@@ -1,12 +1,31 @@
 "use strict"; //ES5
 
 //Спрашиваем имя, создаем переменную пароля
-let userName = prompt('Кто там? \n\nПодсказка: Админ','');
+let userName = prompt('Шаг 1. Кто там? \n\nПодсказка: Админ','');
 let password;
 
 // Проверяем ввод имени
 // Вызываем функцию Запрос пароля, или возвращаем к запросу имени
-if (userName == null) {
+switch(userName) {
+    case null:
+        alert('Отменено');
+        location.reload();
+        break;
+    case 'Админ':
+        Function();
+        break;
+    case '':
+        alert('Введите имя пользователя');
+        location.reload();
+        break;
+    default:
+        alert('Давайте познакомимся позже =)');
+        location.reload();
+        break;
+}
+
+// Проверка имени; Вариант if else
+/*if (userName == null) {
     alert('Отменено');
     location.reload();
 } else if (userName === 'Админ') {
@@ -17,11 +36,35 @@ if (userName == null) {
 } else {
     alert('Давайте познакомимся позже =)');
     location.reload();
-}
+}*/
 
 // Функция Запрос пароля
 // При неправильном пароле - спросит снова
 function Function() {
+    password = prompt('Шаг 2. Введите пароль \n\nПодсказка: Я Главный','');
+    switch(password) {
+        case null:
+            alert('Отменено');
+            location.reload();
+            break;
+        case 'Я Главный':
+            alert('Добрый день!');
+            alert('Повелитель JavaScript!');
+            location.reload();
+            break;
+        case '':
+            alert('Вы не ввели пароль');
+            Function();
+            break;
+        default:
+            alert('Неверный пароль');
+            Function();
+            break;
+    }
+}
+
+// Функция запрос пароля; Вариант if else
+/*function Function() {
     password = prompt('Введите пароль \n\nПодсказка: Я Главный','');
     if (password == null) {
         alert('Отменено');
@@ -37,9 +80,7 @@ function Function() {
         alert('Неверный пароль');
         Function();
     }
-}
-
-
+}*/
 
 
 
